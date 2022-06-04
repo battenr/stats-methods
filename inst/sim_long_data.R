5e3254fba900e501ec8b5e92ef4505914033b0af
battenr <rmb788@mun.ca>
2022-06-04 12:42:22 -0300

    simulating data similar to PhD project

--- inst/sim_long_data.R
+++ inst/sim_long_data.R
@@ -21,30 +21,43 @@ set.seed(2022)
 
 # First need to make cross-sectional data 
 
-# age, sex, dosage, duration of prescription, province, marital status, smoking status, highest level of education, urban/rural and income
+# Independent variables
 
-tdef <- simstudy::defData(varname = "age", dist = "normal", formula = 45, variance = 10)
-tdef <- simstudy::defData(tdef, varname = "sex", dist = "binary", formula = 0.5)
-tdef <- simstudy::defData(tdef, varname = "bzd", dist = "binary", formula = "logit(0.5+")
-tdef <- simstudy::defData(tdef, varname = "bzd_dosage", dist = "normal", "")
-tdef <- simstudy::defData(tdef, varname = "bzd_duration", dist = "normal",
-tdef <- simstudy::defData(tdef, varname = "education", dist = "categorical", formula = "0.3; 0.2; 0.5")
-tdef <- simstudy::defData(tdef, varname = "urban_rural", dist = "categorical", formula = "0.3; 0.2; 0.5")
-tdef <- simstudy::defData(tdef, varname = "urban_rural", dist = "categorical", formula = "0.3; 0.2; 0.5")
-
-
-
-tdef <- simstudy::defData(tdef, varname = "high.blood.pressure", dist = "binary", formula = 0.5)
+tdef <- simstudy::defData(varname = "age", dist = "normal", formula = 45, variance = 10) %>% 
+  simstudy::defData(varname = "sex", dist = "binary", formula = 0.5) %>% 
+# tdef <- simstudy::defData(tdef, varname = "bzd_dosage", dist = "normal", "1.5*bzd+1.5*age+0.5*sex")
+# parking bzd dosage for now for simplicity
+  simstudy::defData(varname = "province", dist = "categorical", formula = "0.2; 0.2; 0.2; 0.2; 0.2") %>% 
+  simstudy::defData(varname = "marital_status", dist = "categorical", formula = "0.3; 0.3; 0.4") %>% 
+  simstudy::defData(varname = "education", dist = "categorical", formula = "0.3; 0.2; 0.5") %>% 
+  simstudy::defData(varname = "urban_rural", dist = "categorical", formula = "0.7; 0.3") %>% 
+  simstudy::defData(varname = "income", dist = "categorical", formula = "0.2; 0.2; 0.2; 0.2; 0.2") %>% 
+  
+  # Adverse Event 
+  
+  simstudy::defData(varname = "anxiety", dist = "binary", formula = 0.5) %>% 
+  simstudy::defData(varname = "depression", dist = "binary", formula = 0.5) %>% 
+  simstudy::defData(varname = "diabetes_mellitus", dist = "binary", formula = 0.5) %>% 
+  simstudy::defData(varname = "high_blood_pressure", dist = "binary", formula = 0.5) %>% 
+  simstudy::defData(varname = "insomnia", dist = "binary", formula = 0.5) %>% 
+  simstudy::defData(varname = "myocardial_infarction", dist = "binary", formula = 0.5) %>% 
+  simstudy::defData(varname = "stroke", dist = "binary", formula = 0.5) %>% 
+  simstudy::defData(varname = "cancer", dist = "binary", formula = 0.5) %>% 
+  simstudy::defData(varname = "cardiovascular_disease", dist = "binary", formula = 0.5) %>% 
+  simstudy::defData(varname = "copd", dist = "binary", formula = 0.5) %>% 
+  simstudy::defData(varname = "dementia", dist = "binary", formula = 0.5) %>% 
+  simstudy::defData(varname = "infections", dist = "binary", formula = 0.5) %>% 
+  simstudy::defData(varname = "pneumonia", dist = "binary", formula = 0.5) %>% 
 
 # Variables needed for making longitudinal
-
-tdef <- simstudy::defData(tdef, varname = "nCount", dist = "noZeroPoisson", formula = 6)
-tdef <- simstudy::defData(tdef, varname = "mInterval", dist = "gamma", formula = 30, variance = 0.01)
-tdef <- simstudy::defData(tdef, varname = "vInterval", dist = "nonrandom", formula = 0.07)
+  
+  simstudy::defData(varname = "nCount", dist = "noZeroPoisson", formula = 6) %>% 
+  simstudy::defData(varname = "mInterval", dist = "gamma", formula = 30, variance = 0.01) %>% 
+  simstudy::defData(varname = "vInterval", dist = "nonrandom", formula = 0.07)
 
 # Generating cross-sectional data
 
-dtTrial <- simstudy::genData(500, tdef)
+dtTrial <- simstudy::genData(1000, tdef)
 dtTrial
 
 #... Add variable to existing data ----
@@ -60,17 +73,11 @@ dtTime
 #... If Dependent on Covariates ----
 
 formula1 <- c("-2 + 2*sex + .5*age", "-1 - 2*sex + .5*age")
-dtExp <- simstudy::trtObserve(dtTime, formulas = formula1, logit.link = TRUE, grpName = "exposure")
-
-#... Not Dependent on any Covariates 
-
-formula2 <- c(0.35, 0.45)
-
-dtExp2 <- simstudy::trtObserve(dtstudy, formulas = formula2, logit.link = FALSE, grpName = "exposure")
-
-#... Adding dosage ----
-
-def2 <- defDataAdd(varname = "dosage", dist = "normal", formula = "15 + .1 * time + exposure", variance = 5)
-dtPeriod <- addColumns(def2, dtExp)
+dtExp <- simstudy::trtObserve(dtTime, formulas = formula1, logit.link = TRUE, grpName = "bzd")
 
 
+# Update Definitions ----
+
+# Updating definition to include age 
+
+simstudy::updateDef()
