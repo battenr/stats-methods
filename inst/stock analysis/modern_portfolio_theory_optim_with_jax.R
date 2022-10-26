29976a0c58912233359a1c9ebdd26010501b774a
battenr <ryan.batten@mun.ca>
2022-10-26 15:33:17 +0000

    adding way to do modern portfolio theory in R

--- inst/stock analysis/modern_portfolio_theory_optim_with_jax.R
+++ inst/stock analysis/modern_portfolio_theory_optim_with_jax.R
@@ -252,4 +252,197 @@ ggplot(df.aal.cumsum, mapping = aes(x = date, y = logcumsum)) +
 
 # Modern Portfolio Theory ----
 
+# Following this link https://www.codingfinance.com/post/2018-05-31-portfolio-opt-in-r/#:~:text=Portfolio%20optimization%20is%20an%20important,need%20to%20optimize%20the%20portfolios.
 
+library(tidyquant) # To download the data
+library(plotly) # To create interactive charts
+library(timetk) # To manipulate the data series
+library(tidyverse)
+
+#... Data ----
+
+tick <- c('AMZN', 'AAPL', 'NFLX', 'XOM', 'T')
+
+price_data <- tq_get(tick,
+                     from = '2014-01-01',
+                     to = '2018-05-31',
+                     get = 'stock.prices')
+
+#... Daily Returns ----
+
+log_ret_tidy <- price_data %>%
+  group_by(symbol) %>%
+  tq_transmute(select = adjusted,
+               mutate_fun = periodReturn,
+               period = 'daily',
+               col_rename = 'ret',
+               type = 'log')
+
+#... Making Wide Format and XTS format ----
+
+log_ret_xts <- log_ret_tidy %>%
+  spread(symbol, value = ret) %>%
+  tk_xts()
+
+head(log_ret_xts)
+
+#... Mean Return ----
+
+mean_ret <- colMeans(log_ret_xts)
+print(round(mean_ret, 5))
+
+#... Covariance ----
+
+cov_mat <- cov(log_ret_xts) * 252
+
+print(round(cov_mat,4))
+
+?optim
+
+fr <- function(x) {   ## Rosenbrock Banana function
+  x1 <- x[1]
+  x2 <- x[2]
+  100 * (x2 - x1 * x1)^2 + (1 - x1)^2
+}
+
+optim(price_data$close, fr)
+
+#... Portfolio Returns and Risk ----
+
+# Calculate the random weights (dividing by sum ensures they add up to 1)
+wts <- runif(n = length(tick))
+wts <- wts/sum(wts)
+
+# Calculate the portfolio returns
+port_returns <- (sum(wts * mean_ret) + 1)^252 - 1
+
+# Calculate the portfolio risk
+port_risk <- sqrt(t(wts) %*% (cov_mat %*% wts))
+
+# Calculate the Sharpe Ratio
+sharpe_ratio <- port_returns/port_risk
+
+print(wts)
+
+#... Simulating 5000 random portfolios ----
+
+num_port <- 5000
+
+# Creating a matrix to store the weights
+
+all_wts <- matrix(nrow = num_port,
+                  ncol = length(tick))
+
+# Creating an empty vector to store
+# Portfolio returns
+
+port_returns <- vector('numeric', length = num_port)
+
+# Creating an empty vector to store
+# Portfolio Standard deviation
+
+port_risk <- vector('numeric', length = num_port)
+
+# Creating an empty vector to store
+# Portfolio Sharpe Ratio
+
+sharpe_ratio <- vector('numeric', length = num_port)
+
+for (i in seq_along(port_returns)) {
+  
+  wts <- runif(length(tick))
+  wts <- wts/sum(wts)
+  
+  # Storing weight in the matrix
+  all_wts[i,] <- wts
+  
+  # Portfolio returns
+  
+  port_ret <- sum(wts * mean_ret)
+  port_ret <- ((port_ret + 1)^252) - 1
+  
+  # Storing Portfolio Returns values
+  port_returns[i] <- port_ret
+  
+  
+  # Creating and storing portfolio risk
+  port_sd <- sqrt(t(wts) %*% (cov_mat  %*% wts))
+  port_risk[i] <- port_sd
+  
+  # Creating and storing Portfolio Sharpe Ratios
+  # Assuming 0% Risk free rate
+  
+  sr <- port_ret/port_sd
+  sharpe_ratio[i] <- sr
+  
+}
+
+# Storing the values in the table
+portfolio_values <- tibble(Return = port_returns,
+                           Risk = port_risk,
+                           SharpeRatio = sharpe_ratio)
+
+
+# Converting matrix to a tibble and changing column names
+all_wts <- tk_tbl(all_wts)
+
+colnames(all_wts) <- colnames(log_ret_xts)
+
+# Combing all the values together
+portfolio_values <- tk_tbl(cbind(all_wts, portfolio_values))
+
+head(portfolio_values)
+
+min_var <- portfolio_values[which.min(portfolio_values$Risk),]
+max_sr <- portfolio_values[which.max(portfolio_values$SharpeRatio),]
+
+#... Plotting with Lowest Variance ----
+
+p <- min_var %>%
+  gather(AAPL:XOM, key = Asset,
+         value = Weights) %>%
+  mutate(Asset = as.factor(Asset)) %>%
+  ggplot(aes(x = fct_reorder(Asset,Weights), y = Weights, fill = Asset)) +
+  geom_bar(stat = 'identity') +
+  theme_minimal() +
+  labs(x = 'Assets', y = 'Weights', title = "Minimum Variance Portfolio Weights") +
+  scale_y_continuous(labels = scales::percent) 
+
+ggplotly(p)
+
+#... Plotting Portfolio with Highest Sharpe Ratio ----
+
+p <- max_sr %>%
+  gather(AAPL:XOM, key = Asset,
+         value = Weights) %>%
+  mutate(Asset = as.factor(Asset)) %>%
+  ggplot(aes(x = fct_reorder(Asset,Weights), y = Weights, fill = Asset)) +
+  geom_bar(stat = 'identity') +
+  theme_minimal() +
+  labs(x = 'Assets', y = 'Weights', title = "Tangency Portfolio Weights") +
+  scale_y_continuous(labels = scales::percent) 
+
+ggplotly(p)
+
+p <- portfolio_values %>%
+  ggplot(aes(x = Risk, y = Return, color = SharpeRatio)) +
+  geom_point() +
+  theme_classic() +
+  scale_y_continuous(labels = scales::percent) +
+  scale_x_continuous(labels = scales::percent) +
+  labs(x = 'Annualized Risk',
+       y = 'Annualized Returns',
+       title = "Portfolio Optimization & Efficient Frontier") +
+  geom_point(aes(x = Risk,
+                 y = Return), data = min_var, color = 'red') +
+  geom_point(aes(x = Risk,
+                 y = Return), data = max_sr, color = 'red') +
+  annotate('text', x = 0.20, y = 0.42, label = "Tangency Portfolio") +
+  annotate('text', x = 0.18, y = 0.01, label = "Minimum variance portfolio") +
+  annotate(geom = 'segment', x = 0.14, xend = 0.135,  y = 0.01, 
+           yend = 0.06, color = 'red', arrow = arrow(type = "open")) +
+  annotate(geom = 'segment', x = 0.22, xend = 0.2275,  y = 0.405, 
+           yend = 0.365, color = 'red', arrow = arrow(type = "open"))
+
+
+ggplotly(p)
--- inst/stock analysis/mpt_python.py
+++ inst/stock analysis/mpt_python.py
@@ -1,8 +1,10 @@
 from datetime import date
 from datetime import timedelta
+import pandas as pd
 import numpy as np
 import yfinance as yf
-import matplotlib as plot
+import matplotlib as plt
+import torch 
 
 
 tickers = ['BRZE','AAPL','SBUX','AAL','WMT','AMZN',
@@ -13,6 +15,53 @@ start_date = end_date - timedelta(days=120)
 stock_data = yf.download(tickers, start=start_date, end = end_date)
 
 ret_data = np.log(stock_data/stock_data.shift(1))
+
+
+rng = np.random.default_rng(1337)
+X = np.array(ret_data.T)
+w = rng.random(ret_data.shape[1])
+w = torch.nn.Softmax(w)
+
+plt.plot(w
+plot(w).show
+plt.legend
+plt.show()
+
+w = DataFrame(w)
+
+
+
+import jax
+import jax.numpy as jnp
+?jax
+from jax import grad, jit, vmap
+from jax import random
+N = stock_data.shape[0]
+
+jax.nn.softmax(ret_data)
+
+def exp_r_and_cov(ret_m):
+    r = jnp.mean(ret_m, axis=1)
+    cov_m = cov_m = jnp.cov(ret_m)
+    return r, cov_m
+
+def ret_and_vol(w, r, cov_m):
+    #ensure our weights sum to 1
+    w = jax.nn.softmax(w)
+    ex_ret = jnp.dot(w.T,r) * N
+    ex_vol = jnp.sqrt(jnp.dot(w.T, jnp.dot(cov_m * N,w)))
+    return (ex_ret,ex_vol)
+
+def sharpe(ex_ret, ex_vol):
+    sr = ex_ret/ex_vol
+    return sr
+
+def exp_r_and_cov(ret_m):
+    r = jnp.mean(ret_m, axis=1)
+    cov_m = cov_m = jnp.cov(ret_m)
+    return r, cov_m
+
+
 
 ret_data.dropna(inplace=True)
 print
