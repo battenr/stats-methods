031622491a350e7fbcf75f613ca012dd83cf1f09
battenr <rmb788@mun.ca>
2022-11-23 14:39:08 -0400

    adding outline ffor structural nested models

---
+++ inst/what-if-hernan-robins/ch14-g-estimation-snn.R
@@ -0,0 +1,22 @@
+# Title: G-estimation of Structural Nested Models
+
+# Description: 
+
+# working through the R code here:
+
+# https://remlapmot.github.io/cibookex-r/g-estimation-of-structural-nested-models.html
+
+# Setup ----
+
+#... Libraries ----
+
+library(tidyverse)
+library(readr)
+
+#... Dependencies ----
+
+#... Pins ----
+
+# Data ----
+
+nhefs <- readr::read_csv("data/nhefs.csv")
