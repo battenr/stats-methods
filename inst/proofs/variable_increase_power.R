7c4e63e2c94155d59be607981191939034b09b25
battenr <ryan.batten@mun.ca>
2023-07-19 16:27:56 -0300

    adding DAG

--- inst/proofs/variable_increase_power.R
+++ inst/proofs/variable_increase_power.R
@@ -5,11 +5,16 @@
 
 # Description: Trying to simulate data to show that including a continuous variable increases power
 
+# References: Kahan et al. (2014) - risks and rewards...
+
 # Setup ----
 
 #... Libraries ----
 
 library(tidyverse)
+library(ggdag)
+theme_set(theme_dag())
+# library(MASS) # can use for a multivariate normal distribution (kinda need real data first)
 
 #... Functions ----
 
@@ -21,5 +26,17 @@ library(tidyverse)
 
 # DAG ----
 
-# Simulated Data ----
+dag = dagify(
+  y ~ x + C, # assuming continuous outcome to start 
+  # variable c that is prognostic of the outcome 
+  exposure = "x",
+  outcome = "y"
+)
 
+dag |> 
+  ggdag(
+    layout = "star")
+
+ggdag_adjustment_set(dag) # Need to adjust for c since a confounder
+
+# Simulated Data ----
