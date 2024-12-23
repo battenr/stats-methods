d92ead071fa5b92b93836442a7d6b6eccebc6bc5
Ryan Batten <ryan.batten@mun.ca>
2024-12-23 11:14:16 -0400

    final version of code for ROPE

--- R/custom_theme.R
+++ R/custom_theme.R
@@ -1,7 +1,7 @@
 custom_theme <- function() {
   theme_minimal() %+replace% # basing this on the theme_survminer() but editing some of the components
     theme(
-      plot.title = element_text(family = "Jost", face = "bold", size = 20),
+      plot.title = element_text(hjust = 0.5, family = "Jost", face = "bold", size = 20),
       #axis.title = element_text(family = "Jost Medium"),
       plot.subtitle = element_text(hjust = 0.5, size = 16),
       text = element_text(family = "Jost", size = 16)
--- inst/bayes methods/rope.R
+++ inst/bayes methods/rope.R
@@ -1,8 +1,11 @@
-# Title: Demonstration of Region of Practical Equivalence (ROPE)
+# Title: Demonstration of the Region of Practical Equivalence (ROPE)
 
 # Description: Showing how ROPE can be a helpful tool. 
-# My personal preference is to show the entire distribution, however 
-# we can also use ROPE for decision making. 
+# My personal preference is to show the entire distribution, however we can also use ROPE for decision making. 
+
+# This script has two examples: 
+# 1. There is actually an effect (1.5)
+# 2. There is no effect (0)
 
 # Setup ----
 
@@ -15,15 +18,60 @@ library(bayestestR) # for certain things. In this case, for calculating ROPE
 
 #... Functions ----
 
-source("R/custom_theme.R")
-source("R/sim_data.R")
+# Custom Theme 
 
-# Data ----
+# This custom theme is used for the plots 
 
-df <- sim_data()
+custom_theme <- function() {
+  theme_minimal() %+replace% # basing this on the minimal theme with some adjustments
+    theme(
+      plot.title = element_text(hjust = 0.5, 
+                                family = "JostRoman-bold", 
+                                face = "bold", 
+                                size = 30),
+      #axis.title = element_text(family = "Jost Medium"),
+      plot.subtitle = element_text(hjust = 0.5, size = 24),
+      text = element_text(family = "Jost", size = 24)
+    ) 
+}
 
-# df <- df %>%
-#   mutate(across(c(x, z1, z2), scale))
+# Simulating Data Function
+
+sim_data <- function(n = 250, # sample size 
+                     beta_trt = 1.5, # treatment effect
+                     # Parameters for Z1 
+                     z1_mean = 5, z1_sd = 2, 
+                     # Parameters for Z2
+                     z2_size = 1, z2_prob = 0.5, 
+                     # Confounder - Effect on X
+                     z1_on_x = 0.05, z2_on_x = 0.2,
+                     # Confounder - Effect on Y
+                     z1_on_y = 0.5, z2_on_y = 0.3){
+  
+  # Creating the Dataframe
+  
+  df <- data.frame(
+    z1 = rnorm(n = n, mean = z1_mean, sd = z1_sd), 
+    z2 = rbinom(n = n, size = z2_size, prob = z2_prob)
+  ) %>% 
+    dplyr::mutate(
+      prob = plogis(z1_on_x*z1 + z2_on_x*z2), 
+      x = rbinom(n = n, size = 1, prob = prob), 
+      y = beta_trt*x + z1_on_y*z1 + z2_on_y*z2 + rnorm(n = n, mean = 0, sd = 1)
+    )
+  
+  # Return the dataframe
+  
+  return(df)
+}
+
+# Simulating Data ----
+
+set.seed (654) # setting seed for reproducibility
+
+df_with_effect <- sim_data() # a dataset where there really is an effect
+
+df_with_no_effect <- sim_data(beta_trt = 0) # a dataset where there really is no effect
 
 # Bayesian Model ----
 
@@ -35,41 +83,60 @@ priors <- c(
   prior(normal(0, 2), class = "b", coef = "z2")
 )
 
-# Scaling the Variables 
+# Fitting models 
 
+#... Model with an effect
 
+mod1 <- brms::brm(y ~ x + z1 + z2, 
+                  family = gaussian(link = "identity"), 
+                  data = df_with_effect, 
+                  prior = priors)
 
-# Fitting the model 
+pp_check(mod1) # checking posterior distribution
 
-mod <- brms::brm(y ~ x + z1 + z2, 
-                 family = gaussian(link = "identity"), 
-                 data = df, 
-                 prior = priors)
+#... Model with dataset with no effect 
 
-pp_check(mod)
+mod2 <- brms::brm(y ~ x + z1 + z2, 
+                  family = gaussian(link = "identity"), 
+                  data = df_with_no_effect, 
+                  prior = priors)
 
-# Let's Plot the Parameter ----
+pp_check(mod2) # checking posterior distribution
 
-mod %>% 
+# Let's Plot the Parameter! ----
+
+# Here we are just plotting the parameter to see what the results look like
+
+# For when there is an effect
+
+mod1 %>% 
   spread_draws(b_x) %>% 
   ggplot(aes(x = b_x)) +
   stat_halfeye(fill = "pink", 
-             color = "purple") + 
+               color = "purple",
+  ) + 
   labs(x = "Effect Estimate",
        y = "Density") + 
   # Edit the titile
-  ggtitle("Predicted Outcome",
-          subtitle = "Prior ~ N(0,5)") +
-  theme_minimal() +
-  theme(
-    plot.title = element_text(hjust = 0.5, face = "bold"),
-    plot.subtitle = element_text(hjust = 0.5),
-    text = element_text(size = 20)
-  ) 
+  ggtitle("Effect Estimate",
+          subtitle = "Prior ~ N(0,2)") +
+  custom_theme()
 
-percentage_in_rope <- rope(mod, parameters = "x")
+# For when there is no effect 
 
-plot(percentage_in_rope) + custom_theme()
+mod2 %>% 
+  spread_draws(b_x) %>% 
+  ggplot(aes(x = b_x)) +
+  stat_halfeye(fill = "pink", 
+               color = "purple",
+  ) + 
+  labs(x = "Effect Estimate",
+       y = "Density") + 
+  # Edit the titile
+  ggtitle("Effect Estimate",
+          subtitle = "Prior ~ N(0,2)") +
+  custom_theme()
+
 
 # Calculating ROPE ----
 
@@ -77,9 +144,33 @@ plot(percentage_in_rope) + custom_theme()
 # us determine what the region would be where we'd basically say there is no effect. 
 # For example, 0 is no effect. What about 0.3? What about 0.4? 
 
-# Note: this is meant to be an introduction, but there are 
+# Note: this is meant to be an introduction. For more details, recommend the bayestestR vignette here: 
+# https://easystats.github.io/bayestestR/reference/rope.html
 
-# For more information, recommend: 
-# 
+#... When There Is an Effect ----
 
-bayestestR::rope(mod)
+rope(mod1) # we can see what percent is in ROPE
+
+percentage_in_rope <- rope(mod1, parameters = "x")
+
+plot(percentage_in_rope) + 
+  custom_theme() +
+  labs(subtitle = "True Effect is 1.5",
+       x = "Possible parameter values for effect estimate",
+       y = "Density",
+       fill = "Credible Interval")
+
+#... When There is No Effect ----
+
+# For All Parameters: 
+
+rope(mod2) # we can see what percent is in ROPE
+
+percentage_in_rope2 <- rope(mod2, parameters = "x")
+
+plot(percentage_in_rope2) + 
+  custom_theme() +
+  labs(subtitle = "True Effect is 0",
+       x = "Possible parameter values for effect estimate",
+       y = "Density",
+       fill = "Credible Interval")
